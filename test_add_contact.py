12a21a82b83ea931711546a24d19c226e3dc0cd4
PStoiko <pstoiko@gmail.com>
2018-12-12 12:40:34 -0500

    "Фикстура": общий код инициализации и завершения | Done.

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,27 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.open_add_new_contact_page()
-        self.app.fill_in_contact_form(Contact(firstname='First name', middlename='Middlename', lastname='Lastname'))
-
-    def test_add_empty_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.open_add_new_contact_page()
-        self.app.fill_in_contact_form(Contact(firstname='', middlename='', lastname=''))
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def tearDown(self):
-        self.app.destroy()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.open_add_new_contact_page()
+    app.fill_in_contact_form(Contact(firstname='First name', middlename='Middlename', lastname='Lastname'))
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.open_add_new_contact_page()
+    app.fill_in_contact_form(Contact(firstname='', middlename='', lastname=''))
+
--- test_add_group.py
+++ test_add_group.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
 from group import Group
 from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-    
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name='Name group', header='Group header', footer='Group footer'))
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header=" header", footer=""))
-        self.app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name='Name group', header='Group header', footer='Group footer'))
+    app.logout()
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header=" header", footer=""))
+    app.logout()
