16958c04c5179ebcd321a60e7c286db7c76f23bd
PStoiko <pstoiko@gmail.com>
2018-12-14 16:03:31 -0500

    Делаем новые тесты без рекордера | added test_del_group | passed

--- fixture/group.py
+++ fixture/group.py
@@ -26,3 +26,11 @@ class GroupHelper:
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
---
+++ test/test_del_group.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pytest
+from model.group import Group
+from fixture.application import Application
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+def test_delete_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.delete_first_group()
+    app.session.logout()
