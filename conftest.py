613b9a1e80b6ca500928e012c5acc2196ff36268
PStoiko <pstoiko@gmail.com>
2018-12-14 16:55:18 -0500

    Делаем новые тесты без рекордера |test_add_contact passed after scope = session was removed

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fixture.application import Application
 
-@pytest.fixture (scope = 'session')
+@pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
